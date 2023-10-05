bcff065f6e00aa629edba6e7d5afa6176f060a93
Tholkappiar M <114584730+Tholkappiar@users.noreply.github.com>
2023-10-05 20:05:49 +0530

    Add files via upload

---
+++ BackEnd/Day 11/Spam-Shield/src/main/java/com/spam/shield/service/impl/UserServiceImpl.java
@@ -0,0 +1,79 @@
+package com.spam.shield.service.impl;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.spam.shield.repository.UserRepository;
+import com.spam.shield.service.UserService;
+import org.springframework.stereotype.Service;
+
+import com.spam.shield.dto.request.UserRequest;
+import com.spam.shield.dto.response.UserResponse;
+import com.spam.shield.model.User;
+import com.spam.shield.model.enumerate.Role;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public List<UserResponse> getAllUsers() {
+        List<User> userList = userRepository.findAll();
+        return userList.stream()
+                .filter(user -> !user.getRole().equals(Role.ADMIN))
+                .map(this::mapUserToUserResponse)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public UserResponse getUser(Long uid) {
+        User user = userRepository.findByUid(uid);
+        return mapUserToUserResponse(user);
+    }
+
+    @Override
+    public UserResponse updateUser(UserRequest request, Long uid) {
+        User user = userRepository.findByUid(uid);
+        User newUser = new User();
+        if (user != null) {
+            newUser = User.builder()
+                    .name(request.getName())
+                    .email(request.getEmail())
+                    .password(request.getPassword())
+                    .role(request.getRole())
+                    .isEnabled(request.getIsEnabled())
+                    .build();
+            userRepository.save(newUser);
+        }
+        return mapUserToUserResponse(newUser);
+    }
+
+    @Override
+    public boolean deleteProduct(Long uid) {
+        User user = userRepository.findByUid(uid);
+
+        if (user != null) {
+            userRepository.deleteByUid(uid);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private UserResponse mapUserToUserResponse(User user) {
+        return UserResponse.builder()
+                .uid(user.getUid())
+                .name(user.getName())
+                .email(user.getEmail())
+                .role(user.getRole())
+                .isEnabled(user.getIsEnabled())
+                .build();
+    }
+
+}
